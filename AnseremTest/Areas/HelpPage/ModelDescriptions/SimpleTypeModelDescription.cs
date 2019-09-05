26555407f2da8cb69bfd076757f5fa15ddedc2c9
Andrey <dn6pr0@gmail.com>
2019-09-05 12:58:07 +0300

    Добавьте файлы проекта.

---
+++ AnseremTest/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AnseremTest.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AnseremTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnseremTest.WebApiApplication" Language="C#" %>
